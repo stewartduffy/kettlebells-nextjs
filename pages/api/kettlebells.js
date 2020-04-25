67cc72b05187d38218236db0934838637fbf3628
Stewart Duffy <duffy.stewart@gmail.com>
2020-04-25 19:30:53 +1200

    Adds seventhsin

--- pages/api/kettlebells.js
+++ pages/api/kettlebells.js
@@ -8,6 +8,9 @@ const sitesToCheck = [
   "http://solidstrengthequipment.co.nz/4kg-kettlebell",
   "http://solidstrengthequipment.co.nz/12kg-kettlebell",
   "http://solidstrengthequipment.co.nz/16kg-kettlebell",
+
+  "https://www.seventhsin.co.nz/collections/kettlebells/products/16kg-seventh-sin-kettlebell",
+  "https://www.seventhsin.co.nz/collections/kettlebells/products/32kg-seventh-sin-kettlebell",
 ];
 
 const checkUrl = (url, search) => {
@@ -17,17 +20,21 @@ const checkUrl = (url, search) => {
 function getStatus(response, url) {
   // load up cheerio
   const $ = cheerio.load(response.data);
+  let selectorValue = null;
 
   switch (true) {
     case checkUrl(url, "industrialathletic"):
-      return $("#form-action-addToCart").attr("value") === "Add to Cart"
-        ? "Available"
-        : "Out Of Stock";
+      selectorValue = $("#form-action-addToCart").attr("value");
+      return selectorValue === "Add to Cart" ? "Available" : "Out Of Stock";
     case checkUrl(url, "solidstrengthequipment"):
-      return $('.product-add-to-cart input[type="submit"]').attr("value") ===
-        "Add To Cart"
-        ? "Available"
-        : "Out Of Stock";
+      selectorValue = $('.product-add-to-cart input[type="submit"]').attr(
+        "value"
+      );
+      return selectorValue === "Add To Cart" ? "Available" : "Out Of Stock";
+
+    case checkUrl(url, "seventhsin"):
+      selectorValue = $("span.productlabel.soldout").text();
+      return selectorValue === "Sold Out" ? "Out Of Stock" : "Available";
   }
 }
 
